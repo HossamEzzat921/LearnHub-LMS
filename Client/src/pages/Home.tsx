fc33f402d3220c872a2480898a19fa7ee0e18bf2
hossamezzat921 <hossamezzat921@gmail.com>
2026-06-07 19:02:36 +0300

    Fix-1

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -135,8 +135,8 @@ const Home = () => {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {featuredCourses.map((course, index) => (
-              <CourseCard key={course.id} course={course} index={index} />
+  {featuredCourses?.map((course, index) => (
+  <CourseCard key={course.id} course={course} index={index} />
             ))}
           </div>
 
